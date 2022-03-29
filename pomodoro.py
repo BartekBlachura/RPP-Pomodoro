0733b21f5e469fc5127aad9174225fc79ef7131c
BartekBlachura <blachurab@gmail.com>
2022-03-29 22:25:56 +0200

    29.03.22

--- pomodoro.py
+++ pomodoro.py
@@ -1,19 +1,39 @@
 import machine
 import utime
 
+
+# zmienić odliczneie na odzliczenie w dół
+def pomodoro_time():
+    stopwatch = 0  # seconds
+    pomodoro_stopwatch = 10  # seconds
+
+    while stopwatch < pomodoro_stopwatch:
+        stopwatch += 1 / pomodoro_stopwatch
+        utime.sleep(1 / pomodoro_stopwatch)
+        print(stopwatch)
+
+        if stopwatch > pomodoro_stopwatch:
+            # print(utime.time() - start_time)
+            return
+
+
+def rest_time():
+    pass
+
+
 button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 builtin_led = machine.Pin(25, machine.Pin.OUT)
 led_external = machine.Pin(15, machine.Pin.OUT)
 
 pomodoro_phase = False
-pomodoro_stopwatch = 25  # seconds
+# pomodoro_stopwatch = 25  # seconds
 pomodoro_count = 0
 
 rest_phase = False
 rest_stopwatch = 5  # seconds
 
-stopwatch = 0  # seconds
+# stopwatch = 0  # seconds
 
 builtin_led.value(0)
 led_external.value(0)
@@ -22,22 +42,18 @@ start_time = utime.time()
 while True:
     if not pomodoro_phase and not rest_phase:
         builtin_led.value(1)
+
         if button.value() == 1:
             pomodoro_phase = True
             builtin_led.value(0)
-            start_time = utime.time()
+            # start_time = utime.time()
 
     if pomodoro_phase:
         led_external.value(1)
-        while stopwatch < pomodoro_stopwatch:
-            stopwatch += 1 / pomodoro_stopwatch
-            utime.sleep(1 / pomodoro_stopwatch)
-            print(stopwatch)
+        pomodoro_time()
+        pomodoro_count += 1
+        led_external.value(0)
+        pomodoro_phase = False
 
-        if stopwatch > pomodoro_stopwatch:
-            stopwatch = 0
-            pomodoro_phase = False
-            led_external.value(0)
-            print(utime.time() - start_time)
     if rest_phase:
         pass
